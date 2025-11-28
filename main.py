67d9429d08cc619d6baa8baca3a96d1b99d4adb0
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2025-11-28 20:15:27 +0000

    starting

---
+++ main.py
@@ -0,0 +1,35 @@
+print("wlecome to scioly idetifier thingy")
+print()
+print()
+print()
+print("what do you want to identify rn?")
+print("1. liquids" \
+"2. solids" \
+"3. Metals" \
+"4. Fibers" \
+"5. Hairs" \
+"6. Plastics" \
+"7. mixtures of solids")
+
+def liquids():
+    
+
+
+
+analysis_num=str(input("what do you want to identify rn?")) == 1:
+
+
+if analysis_num == 1:
+    liquids() 
+elif analysis_num == 2:
+    solids()
+elif analysis_num == 3:
+    metals()
+elif analysis_num == 4:
+    fibers()
+elif analysis_num == 5:
+    hairs()
+elif analysis_num == 6:
+    plastics()
+elif analysis_num == 7:
+    mixtures()
